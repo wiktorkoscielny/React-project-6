3cb32e51ae5a3bf01fe3acaf6b1d2e8eb63cda7b
Wiktor Kościelny <81425551+wiktorkoscielny@users.noreply.github.com>
2022-06-28 13:01:21 +0200

    Created Navbar component to switch routes

---
+++ src/components/Navbar.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import './Navbar.css'
+
+
+
+function Navbar() {
+
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => {
+        setClick(!click)
+    };
+
+    const closeMobileMenu = () => {
+        setClick(false);  
+    }
+
+    return (
+        <>
+            <nav className='navbar'>
+                <Link to='/' 
+                    className='navbar-logo'>
+                    TravelMode <i className='fab fa-freebsd' />
+                </Link>
+                <div className='menu-icon' onClick={handleClick}>
+                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                </div>
+
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                            Home
+                        </Link>
+                    </li>
+
+                    <li className='nav-item'>
+                        <Link to='/search' className='nav-links' onClick={closeMobileMenu}>
+                            Search
+                        </Link>
+                    </li>
+                </ul>
+
+            </nav>
+        </>
+    )
+
+}
+export default Navbar
